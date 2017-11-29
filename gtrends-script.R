2278e7d41b6ebaa71991d4bf7954df4327b7c102
ClaraGBouyssou <33460282+ClaraGBouyssou@users.noreply.github.com>
2017-11-29 18:13:36 +0100

    I commit

--- gtrends-script.R
+++ gtrends-script.R
@@ -2,13 +2,16 @@ library(gtrendsR)
 library(ggplot2)
 library(tidyverse)
 
-
+#We are creatin a dataframe using the package gtrendsR
 bitcoin.trend <- gtrends(c("bitcoin"), gprop = "web", time = "all")[[1]]
+#We check we created a data.frame
 class(bitcoin.trend)
+#And we show the first rows
 head(bitcoin.trend)
-
+#We check the class of the variable date in bitcoin dataframe
 class(bitcoin.trend$date)
 
+#We plot a line with date in x and hits in y
 ggplot(data = bitcoin.trend) + geom_line(mapping = aes(x= date, y = hits))
 ?gtrends
 
